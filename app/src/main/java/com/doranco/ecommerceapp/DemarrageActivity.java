6061149446f81ffd4e33ffb1a84009f07db960e0
Sastem <sahai.sarah94@gmail.com>
2021-09-03 07:29:04 -0400

    DemarrageActivity implemented

--- app/src/main/java/com/doranco/ecommerceapp/DemarrageActivity.java
+++ app/src/main/java/com/doranco/ecommerceapp/DemarrageActivity.java
@@ -2,7 +2,9 @@ package com.doranco.ecommerceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class DemarrageActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class DemarrageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demarrage);
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),
+                        MainActivity.class);
+                startActivity(intent);
+                finish(); }
+        };
+        new Handler().postDelayed(runnable, 5000);
     }
 }
